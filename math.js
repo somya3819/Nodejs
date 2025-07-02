a7690b2c9a65aaa4741d3520b4e941ec2471fd9f
somya3819 <somyamahajan597@gmail.com>
2025-07-02 13:22:37 +0530

    day 1

--- math.js
+++ math.js
@@ -1,5 +1,8 @@
+//ES Module
 export function add(a,b){
     return a+b;
 }
-//to export the file:
+
+
+//to export the file: //common js
 // module.exports={add}
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ console.log("hello somya")
 //"npm init -y" for importing/creating packages
 
 
-//for importing :
+//for importing ://common js
 //const math = require('./math.js');
 // console.log(math.add(2,3));
 
+
+//ES module
 import {add} from './math.js'
 console.log(add(2,3))
