a65bc229f48300784fd82aa66199825d61c3a2c5
somya3819 <somyamahajan597@gmail.com>
2025-07-02 13:18:57 +0530

    initial commit

---
+++ math.js
@@ -0,0 +1,5 @@
+export function add(a,b){
+    return a+b;
+}
+//to export the file:
+// module.exports={add}
---
+++ server.js
@@ -0,0 +1,10 @@
+console.log("hello somya")
+//"npm init -y" for importing/creating packages
+
+
+//for importing :
+//const math = require('./math.js');
+// console.log(math.add(2,3));
+
+import {add} from './math.js'
+console.log(add(2,3))
